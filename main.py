b4c6affd8e4af66c0d36eee8f44f7a905f8ae803
merav1 <meravlu189@gmail.com>
2022-06-13 23:53:45 +0300

    change procfile 111

--- main.py
+++ main.py
@@ -163,5 +163,5 @@ def hello_world():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT',5000))
     # run app in debug mode on port 5000
-    app.run(debug=True, port=5000)
-    #serve(app,host="0.0.0.0",port=port)
+    #app.run(debug=True, port=5000)
+    serve(app,host="0.0.0.0",port=port)
